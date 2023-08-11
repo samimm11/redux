c316f3bc2b1d8a816bd96c62b25bd72c21502bc6
samimm11 <sam.imm11@gmail.com>
2023-08-11 12:42:56 +0530

    Added components

--- reduxdemo/src/App.js
+++ reduxdemo/src/App.js
@@ -1,11 +1,21 @@
 import Product from "./components/Product";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {createBrowserRouter,createRoutesFromElements,RouterProvider,Route} from 'react-router-dom'
+import Dashboard from "./components/Dashboard";
+import RootLayout from "./components/RootLayout";
+import Cart from "./components/Cart";
 
 
 function App() {
+  const router=createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<RootLayout></RootLayout>}>
+      <Route index element={<Dashboard></Dashboard>}></Route>
+      <Route path="/cart" element={<Cart></Cart>}></Route>
+    </Route>
+  ))
   return (
     <div className="App">
-      <Product></Product>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
